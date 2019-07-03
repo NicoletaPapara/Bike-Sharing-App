0278bd5751c6bec83f56c9f483606f6984015484
NicoletaPapara <48495860+NicoletaPapara@users.noreply.github.com>
2019-07-04 00:01:31 +0300

    updated to spring boot project

---
+++ src/test/java/com/app/bikesharing/BikesharingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.bikesharing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BikesharingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
