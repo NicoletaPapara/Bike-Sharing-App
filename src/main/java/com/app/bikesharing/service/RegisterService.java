15c1bc0dc4e1c1c73c736a1d55c9bb359882a2c4
nav2110 <alexvirastau@gmail.com>
2019-07-14 17:36:10 +0300

    First commit to register branch

---
+++ src/main/java/com/app/bikesharing/service/RegisterService.java
@@ -0,0 +1,10 @@
+package com.app.bikesharing.service;
+
+import com.app.bikesharing.model.Bike;
+import com.app.bikesharing.model.User;
+
+public class RegisterService {
+    private User user;
+    private Bike bike;
+
+}
