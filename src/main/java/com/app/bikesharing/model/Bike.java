548af74e4a551e28d5d36550343366ec00d7d348
miron.silviu <miron.silviu.ut@gmail.com>
2019-07-30 22:27:59 +0300

    Add bike construction phase

--- src/main/java/com/app/bikesharing/model/Bike.java
+++ src/main/java/com/app/bikesharing/model/Bike.java
@@ -3,11 +3,21 @@ package com.app.bikesharing.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Setter
 @Getter
 public class Bike {
+
+    @Id
+    @GeneratedValue
     private int id;
-    private int image;
+    private int userId;
+    // Blob type file for image
+    private byte[] image;
     private BikeType type;
     private Size size;
     private double price;
