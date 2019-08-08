d37e53513ad92bce681963330803ccfff593c77e
nav2110 <alexvirastau@gmail.com>
2019-08-08 12:56:09 +0300

    Implementation classes

--- src/main/java/com/app/bikesharing/dao/RoleDAO.java
+++ src/main/java/com/app/bikesharing/dao/RoleDAO.java
@@ -4,6 +4,7 @@ import com.app.bikesharing.model.Role;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository//To autowire the components
 public interface RoleDAO extends  CrudRepository<Role, Integer> {
     public Role findByRole(String role);
 }
